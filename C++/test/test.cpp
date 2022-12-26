9327e1990c1c1ad9b4ad08478ad9f061a6bab75f
haoyang9804 <haoyanghkust@gmail.com>
2022-12-26 22:48:16 +0800

    add test for lowering and Nameless

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -7,24 +7,22 @@ int main() {
 
   Expr::Cst* cst1 = new Expr::Cst(2);
   Expr::Var* var1 = new Expr::Var("var1");
-  Expr::Add* add1 = new Expr::Add(cst1, var1);
+  Expr::Add* add1 = new Expr::Add(var1, cst1);
   Expr::Cst* cst2 = new Expr::Cst(4);
   Expr::Mul* mul1 = new Expr::Mul(add1, cst2);
   Expr::Cst* cst3 = new Expr::Cst(5);
   Expr::Let* let1 = new Expr::Let("var1", cst3, mul1);
+  Expr::Var* var2 = new Expr::Var("var2");
+  Expr::Mul* mul2 = new Expr::Mul(var2, let1);
+  Expr::Cst* cst4 = new Expr::Cst(2);
+  Expr::Let* let2 = new Expr::Let("var2", cst4, mul2);
   
   // test Expr
-  if (Expr::eval(let1, {}) != 28) {
-    throw std::logic_error("eval(let1, {} is not 28");
-  }
-  std::cout << "Expression is \"" << Expr::to_str(let1) << "\"" << std::endl;
-  std::cout << "eval shold be 28: " << Expr::eval(let1, {}) << std::endl;
+  std::cout << "Expression is \"" << Expr::to_str(let2) << "\"" << std::endl;
+  std::cout << "eval shold be: " << Expr::eval(let2, {}) << std::endl;
 
   // test Nameless
-  Nameless::Expr* nLet1 = Compiler::lowerFromExprToNameless(let1, {});
-  if (Nameless::eval(nLet1, {}) != 28) {
-    throw std::logic_error("eval(let1, {} is not 28");
-  } 
-  std::cout << "Expression is \"" << Nameless::to_str(nLet1) << "\"" << std::endl;
-  std::cout << "eval should be 28: " << Nameless::eval(nLet1, {}) << std::endl; 
+  Nameless::Expr* nLet2 = Compiler::lowerFromExprToNameless(let2, {});
+  std::cout << "Expression is \"" << Nameless::to_str(nLet2) << "\"" << std::endl;
+  std::cout << "eval should be: " << Nameless::eval(nLet2, {}) << std::endl; 
 }
